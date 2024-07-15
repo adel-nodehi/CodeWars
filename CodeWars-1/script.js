52aa429c370750e6305af370764d396d95cab359
Adel <adelnmo2003@gmail.com>
2024-07-15 22:05:10 +0330

    Highest Scoring Word

--- CodeWars-1/script.js
+++ CodeWars-1/script.js
@@ -411,3 +411,34 @@ Array.prototype.sameStructureAs = function (other) {
 
 // console.log([1, 2].sameStructureAs([[3], 3]), '[1,2] not same as [[3],3]');
 */
+
+function getWordScore(word) {
+  return [...word.toLowerCase()].reduce(
+    (acc, letter) => acc + (letter.charCodeAt(0) - 96),
+    0
+  );
+}
+
+function high(x) {
+  let highScore = 0;
+
+  return x.split(' ').reduce((acc, word) => {
+    const wordScore = getWordScore(word);
+
+    if (highScore < wordScore) {
+      highScore = wordScore;
+      return word;
+    }
+
+    return acc;
+  }, '');
+}
+
+console.log(high('man i need a taxi up to ubud'), 'taxi');
+console.log(high('what time are we climbing up the volcano'), 'volcano');
+console.log(high('take me to semynak'), 'semynak');
+console.log(high('aa b'), 'aa');
+console.log(high('b aa'), 'b');
+console.log(high('bb d'), 'bb');
+console.log(high('d bb'), 'd');
+console.log(high('aaa b'), 'aaa');
